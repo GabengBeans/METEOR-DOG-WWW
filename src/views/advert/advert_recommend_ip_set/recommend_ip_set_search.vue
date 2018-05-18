43f90e426f448d8b23a2fac3c37447e9b9894217
GabengBeans <daxu0403@163.com>
2018-05-18 19:10:08 +0800

    aa

--- src/views/advert/advert_recommend_ip_set/recommend_ip_set_search.vue
+++ src/views/advert/advert_recommend_ip_set/recommend_ip_set_search.vue
@@ -1,58 +1,84 @@
 <template>
-    <div>
-        <Form label-position="right" :label-width="80" class="search">
-            <Row>
-                <Col :xs='24' :sm='12' :md='8' :lg='6'>
-                <FormItem label="服务ID">
-                    <Input clearable style="min-width:100px" v-model="searchServiceId" />
-                </FormItem>
-                </Col>
-                <Col :xs='24' :sm='12' :md='8' :lg='6'>
-                <FormItem label="标题">
-                    <Input clearable style="min-width:100px" v-model="searchName" />
-                </FormItem>
-                </Col>
-                <Col :xs='24' :sm='12' :md='8' :lg='6'>
-                <Button style='margin-left:38px' type="primary" shape="circle" icon="ios-search" @click="searchRecommendIp">搜索</Button>
-                </Col>
-                <Col :xs='24' :sm='12' :md='8' :lg='6'>
-                <Button type="success" @click="showAddRecommendIp=true">添加推荐IP</Button>
-                </Col>
-            </Row>
-        </Form>
-        <Modal v-model="showAddRecommendIp" width="800">
-            <p slot="header" style="color:#f60;text-align:center">
-                <span>添加推荐IP</span>
-            </p>
+  <div>
+    <Form label-position="right" :label-width="80" class="search">
+      <Row>
+        <Col :xs='24' :sm='12' :md='8' :lg='6'>
+        <FormItem label="服务ID">
+          <Input clearable style="min-width:100px" v-model="searchServiceId" />
+        </FormItem>
+        </Col>
+        <Col :xs='24' :sm='12' :md='8' :lg='6'>
+        <FormItem label="标题">
+          <Input clearable style="min-width:100px" v-model="searchName" />
+        </FormItem>
+        </Col>
+        <Col :xs='24' :sm='12' :md='8' :lg='6'>
+        <Button style='margin-left:38px' type="primary" shape="circle" icon="ios-search" @click="searchRecommendIp">搜索</Button>
+        </Col>
+        <Col :xs='24' :sm='12' :md='8' :lg='6'>
+        <Button type="success" @click="showAddRecommendIp=true">添加推荐IP</Button>
+        </Col>
+      </Row>
+    </Form>
+    <Modal v-model="showAddRecommendIp" width="800" :mask-closable="false">
+      <p slot="header" style="color:#f60;text-align:center">
+        <span>添加推荐IP</span>
+      </p>
+      <div>
+        <Form label-position="right" :label-width="80">
+          <FormItem label="服务ID">
+            <Input clearable v-model="serviceId" style="min-width:100px;width:200px" />
+            <Button type="success" style="margin-left:1vw" @click="getServiceDetail">服务详情</Button>
+          </FormItem>
+          <FormItem label="序号">
+            <Input clearable v-model="adSort" style="min-width:100px;width:200px" />
+          </FormItem>
+          <FormItem label="标题">
+            <Input clearable v-model="serviceName" style="min-width:100px;width:200px" />
+          </FormItem>
+          <FormItem label="推广图片">
             <div>
-                <Form label-position="right" :label-width="80">
-                    <FormItem label="服务ID">
-                        <Input clearable v-model="serviceId" style="min-width:100px;width:200px" />
-                    </FormItem>
-                    <FormItem label="序号">
-                        <Input clearable v-model="adSort" style="min-width:100px;width:200px" />
-                    </FormItem>
-                    <FormItem label="标题">
-                        <Input clearable v-model="serviceName" style="min-width:100px;width:200px" />
-                    </FormItem>
-                    <FormItem label="推广图片">
-                        <div>
-                            <uploadSingleImg :imgUrl="imgObj1" :upload="true" :change="true" :detail="true"></uploadSingleImg>
-                            <uploadSingleImg :imgUrl="imgObj2" :upload="true" :change="true" :detail="true"></uploadSingleImg>
-                            <uploadSingleImg :imgUrl="imgObj3" :upload="true" :change="true" :detail="true"></uploadSingleImg>
-                            <uploadSingleImg :imgUrl="imgObj4" :upload="true" :change="true" :detail="true"></uploadSingleImg>
-                            <uploadSingleImg :imgUrl="imgObj5" :upload="true" :change="true" :detail="true"></uploadSingleImg>
-                            <uploadSingleImg :imgUrl="imgObj6" :upload="true" :change="true" :detail="true"></uploadSingleImg>
-                        </div>
-                    </FormItem>
-                </Form>
+              <uploadSingleImg :imgUrl="imgObj1" :upload="true" :change="true" :detail="true"></uploadSingleImg>
+              <uploadSingleImg :imgUrl="imgObj2" :upload="true" :change="true" :detail="true"></uploadSingleImg>
+              <uploadSingleImg :imgUrl="imgObj3" :upload="true" :change="true" :detail="true"></uploadSingleImg>
+              <uploadSingleImg :imgUrl="imgObj4" :upload="true" :change="true" :detail="true"></uploadSingleImg>
+              <uploadSingleImg :imgUrl="imgObj5" :upload="true" :change="true" :detail="true"></uploadSingleImg>
+              <uploadSingleImg :imgUrl="imgObj6" :upload="true" :change="true" :detail="true"></uploadSingleImg>
             </div>
-            <div slot="footer" style="text-align:center">
-                <Button type="success" @click="saveAddRecommendIp">保存</Button>
-                <Button type="error" style="margin-left:3vw">取消</Button>
+          </FormItem>
+        </Form>
+      </div>
+      <div slot="footer" style="text-align:center">
+        <Button type="success" @click="saveAddRecommendIp">保存</Button>
+        <Button type="error" style="margin-left:3vw" @click="showAddRecommendIp=false">取消</Button>
+      </div>
+    </Modal>
+    <Modal v-model="showServiceDetail" width="800" :mask-closable="false">
+      <p slot="header" style="color:#f60;text-align:center">
+        <span>服务详情</span>
+      </p>
+      <div>
+        <Form label-position="right" :label-width="80">
+          <FormItem label="服务标题">
+            <span>asdfsadfadsfdsdf</span>
+          </FormItem>
+          <FormItem label="推广图片">
+            <div>
+              <uploadSingleImg :imgUrl="imgObj1" :upload="true" :change="true" :detail="true"></uploadSingleImg>
+              <uploadSingleImg :imgUrl="imgObj2" :upload="true" :change="true" :detail="true"></uploadSingleImg>
+              <uploadSingleImg :imgUrl="imgObj3" :upload="true" :change="true" :detail="true"></uploadSingleImg>
+              <uploadSingleImg :imgUrl="imgObj4" :upload="true" :change="true" :detail="true"></uploadSingleImg>
+              <uploadSingleImg :imgUrl="imgObj5" :upload="true" :change="true" :detail="true"></uploadSingleImg>
+              <uploadSingleImg :imgUrl="imgObj6" :upload="true" :change="true" :detail="true"></uploadSingleImg>
             </div>
-        </Modal>
-    </div>
+          </FormItem>
+        </Form>
+      </div>
+      <div slot="footer" style="text-align:center">
+        <Button type="error" @click="showServiceDetail=false">关闭</Button>
+      </div>
+    </Modal>
+  </div>
 </template>
 <script>
 import util from "@/libs/util";
@@ -64,6 +90,7 @@ export default {
       searchServiceId: "",
       searchName: "",
       showAddRecommendIp: false,
+      showServiceDetail:false,
       serviceId: "",
       adSort: "",
       serviceName: "",
@@ -135,7 +162,7 @@ export default {
           if (res.data.success) {
             this.showAddRecommendIp = false;
             this.init();
-            this.searchRecommendIp()
+            this.searchRecommendIp();
             this.$Message.destroy();
             this.$Message.success("保存成功！");
           } else {
@@ -150,12 +177,15 @@ export default {
     searchRecommendIp: function() {
       let data = {
         serviceId: this.searchServiceId,
-        name:this.searchName
+        name: this.searchName
       };
       this.$store.commit("GET_RECOMMEND_FOR_PAGE_INFO", {
         data: data,
         pageNo: this.$store.state.app.recommend_ip_public_page
       });
+    },
+    getServiceDetail() {
+      this.showServiceDetail=true
     }
   },
   components: {
@@ -164,6 +194,5 @@ export default {
 };
 </script>
 <style>
-
 </style>
 
