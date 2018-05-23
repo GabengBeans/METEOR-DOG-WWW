b691d1f53fdcaf95afdcefeb628eab1b8d8fdce1
GabengBeans <daxu0403@163.com>
2018-05-23 10:43:00 +0800

    渠道详情添加选择日期

--- src/views/audit_operation/audit_demand/audit_demand_detail.vue
+++ src/views/audit_operation/audit_demand/audit_demand_detail.vue
@@ -1,5 +1,21 @@
 <template>
     <div>
+        <div class="search">
+            <Row>
+                <Col span="12">
+                <Form label-position="right" :label-width="75">
+                    <FormItem style="min-width:100px" label="选择日期">
+                        <DatePicker type="date" 
+                        :value="$route.query.date" 
+                        placeholder="选择日期" 
+                        style="width: 200px"
+                        @on-change="changeDate"
+                        ></DatePicker>
+                    </FormItem>
+                </Form>
+                </Col>
+            </Row>
+        </div>
         <auditUserDetailTable
         :columns="data"
         :data="result"
@@ -22,6 +38,31 @@ export default {
     components:{
         auditUserDetailTable
     },
+    methods:{
+      changeDate(date){
+         let data = {
+      dateday: date
+    };
+    //console.log(data);
+    util
+      .ajax({
+        method: "post",
+        url: base_uri.search_statistics_channel_daily_info_url,
+        data: data
+      })
+      .then(resp => {
+        //console.log(resp);
+        if (resp.data.success) {
+          this.result = resp.data.data;
+        } else {
+          this.$Message.error("信息获取失败");
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+      }
+  },
     created(){
         let data = {
             dateday:this.$route.query.date
@@ -47,5 +88,5 @@ export default {
 </script>
 
 <style>
-
+@import "../../../styles/public.less";
 </style>
--- src/views/audit_operation/audit_user/audit_user_detail.vue
+++ src/views/audit_operation/audit_user/audit_user_detail.vue
@@ -1,51 +1,91 @@
 <template>
     <div>
-        <auditUserDetailTable
-        :columns="data"
-        :data="result"
-        ></auditUserDetailTable>
+        <div class="search">
+            <Row>
+                <Col span="12">
+                <Form label-position="right" :label-width="75">
+                    <FormItem style="min-width:100px" label="选择日期">
+                        <DatePicker type="date" 
+                        :value="$route.query.date" 
+                        placeholder="选择日期" 
+                        style="width: 200px"
+                        @on-change="changeDate"
+                        ></DatePicker>
+                    </FormItem>
+                </Form>
+                </Col>
+            </Row>
+        </div>
+        <auditUserDetailTable :columns="data" :data="result"></auditUserDetailTable>
     </div>
 </template>
 
 <script>
-import config from "../config"
-import util from "@/libs/util"
-import base_uri from "@/libs/base_uri"
-import auditUserDetailTable from "@/views/public-components/table"
+import config from "../config";
+import util from "@/libs/util";
+import base_uri from "@/libs/base_uri";
+import auditUserDetailTable from "@/views/public-components/table";
 export default {
-    data(){
-        return{
-            data:config.auditUserDetailTableColumns,
-            result:[]
+  data() {
+    return {
+      data: config.auditUserDetailTableColumns,
+      result: []
+    };
+  },
+  components: {
+    auditUserDetailTable
+  },
+  methods:{
+      changeDate(date){
+         let data = {
+      dateday: date
+    };
+    //console.log(data);
+    util
+      .ajax({
+        method: "post",
+        url: base_uri.search_statistics_channel_daily_info_url,
+        data: data
+      })
+      .then(resp => {
+        //console.log(resp);
+        if (resp.data.success) {
+          this.result = resp.data.data;
+        } else {
+          this.$Message.error("信息获取失败");
         }
-    },
-    components:{
-        auditUserDetailTable
-    },
-    created(){
-        let data = {
-            dateday:this.$route.query.date
+      })
+      .catch(err => {
+        console.log(err);
+      });
+      }
+  },
+  created() {
+    let data = {
+      dateday: this.$route.query.date
+    };
+    //console.log(data);
+    util
+      .ajax({
+        method: "post",
+        url: base_uri.search_statistics_channel_daily_info_url,
+        data: data
+      })
+      .then(resp => {
+        //console.log(resp);
+        if (resp.data.success) {
+          this.result = resp.data.data;
+        } else {
+          this.$Message.error("信息获取失败");
         }
-        console.log(data)
-        util.ajax({
-            method:'post',
-            url:base_uri.search_statistics_channel_daily_info_url,
-            data:data
-        }).then(resp=>{
-            console.log(resp)
-            if(resp.data.success)
-            {
-                this.result = resp.data.data
-            }else{
-                this.$Message.error("信息获取失败")
-            }
-        }).catch(err=>{
-            console.log(err)
-        })
-    }
-}
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+};
 </script>
 
 <style>
-
+@import "../../../styles/public.less";
 </style>
