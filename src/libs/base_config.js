edc31e902f188eccb52c37321777f7b47d222b63
GabengBeans <daxu0403@163.com>
2018-07-12 18:21:20 +0800

    aa

--- src/libs/base_config.js
+++ src/libs/base_config.js
@@ -5,8 +5,8 @@ let base_config = {
     //测试服务器接口地址
     testUrl: 'http://39.106.211.153:8890',
     //生产接口地址
-    //proUrl:'http://39.106.16.60:8822',
-    proUrl:'https://shootingstargo.com',
+    proUrl:'http://39.106.16.60:8822',
+    //proUrl:'https://shootingstargo.com',
     //阿里图片服务器地址
     imgUrl: 'http://dedc-statics.oss-cn-beijing.aliyuncs.com',
     //百度地图地址
