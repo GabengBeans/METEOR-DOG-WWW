9091ecad9cb51f9f2b68a9faea3c00367ae2c02c
GabengBeans <daxu0403@163.com>
2018-08-10 18:07:51 +0800

    aa

--- src/views/login.vue
+++ src/views/login.vue
@@ -46,8 +46,8 @@ export default {
   data() {
     return {
       form: {
-        userName: "admin",
-        password: "superman",
+        userName: "",
+        password: "",
       },
       rules: {
         userName: [
